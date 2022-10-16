d3d11b4cb463e0c5681515163e2d9d4b1c43add4
itwaky <eroscovic@gmail.com>
2022-10-16 17:03:06 +0200

    A

---
+++ cours/Cours7.py
@@ -0,0 +1,64 @@
+#--------------------Cours----
+
+def fact(n):
+    print("slt")
+    if n==0:
+        return 1
+    else:
+        return n * fact(n-1)
+
+#print(fact(5))
+
+#-----7.5
+
+
+#2)   5
+
+#3)    jusqu'au max récursion
+
+#4) 0, max, return 3 * n
+
+
+
+
+
+
+#-------------------7.6
+
+#-----------1
+
+def puissance(x,y):
+    if y == 0 :
+        return 1
+    else :
+        return x * puissance(x, y-1)
+
+print(puissance(2,5))
+
+
+#----------------------2
+
+
+def multiplication(x,y):
+    if y == 0:
+        return 0
+    else:
+        return x + multiplication(x, y-1)
+
+
+print(multiplication(10, 0))
+
+
+
+#-----------------3
+
+
+def pgcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return a % pgcd(a, b-1)
+
+
+
+print(pgcd(60,100))
