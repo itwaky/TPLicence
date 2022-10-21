3c7c25cd58420edc2000d16aaff875c572170d46
itwaky <eroscovic@gmail.com>
2022-10-21 23:53:16 +0200

    slt

--- tpPython/tp5/tp5.py
+++ tpPython/tp5/tp5.py
@@ -7,7 +7,7 @@ laby = Labyrinthe.creer(11,15)
 for ligne in laby:
     print(ligne)
 
-
+#---------------------------------1.2
 
 def coord(lst, n):
     for y in range(len(lst)):
@@ -16,6 +16,9 @@ def coord(lst, n):
                 return (y,x)
     return None
 
+
+
+#------------------------------------------1.3
 
 def entree(lst):
     return coord(lst, 2)
@@ -28,6 +31,9 @@ def sortie(lst):
 
 print("entre :", entree(laby), " sortie : ", sortie(laby))
 
+
+
+#---------------------------------------------2.1
 
 
 def taille_laby(lst):
@@ -47,6 +53,10 @@ DimensionsY = taille_laby(laby)[0]
 DimensionsX = taille_laby(laby)[1]
 
 
+#---------------------------------------2.3
+
+
+
 def voisin_laby_fin(lgn,col,nb_lignes,nb_colonnes):
     coord = []
     coordNonTeste = [[lgn-1,col],[lgn+1,col],[lgn,col-1],[lgn ,col+1]]
@@ -57,6 +67,11 @@ def voisin_laby_fin(lgn,col,nb_lignes,nb_colonnes):
 
 
 print("cases voisines : ", voisin_laby_fin(1,1,DimensionsX,DimensionsY))
+
+
+
+#----------------------------------------2.4 et 2.5 
+
 
 
 #def voisins_laby_acc(lgn,col,laby):
@@ -75,8 +90,8 @@ def voisins_laby_acc(coord,laby):
     DimensionsY = taille_laby(laby)[0]
     DimensionsX = taille_laby(laby)[1]
     casesVoisines = voisin_laby_fin(coord[0],coord[1],DimensionsX,DimensionsY)
-    for i in range(4):
-        if laby[casesVoisines[i][0]][casesVoisines[i][1]]:
+    for i in range(len(casesVoisines)):
+        if laby[casesVoisines[i][0]][casesVoisines[i][1]] == 1 or laby[casesVoisines[i][0]][casesVoisines[i][1]] == 3:
             casesAcc = casesAcc + [casesVoisines[i]]
     return casesAcc
 
@@ -86,12 +101,28 @@ print("cases accessibles : ", voisins_laby_acc((3,4),laby))
 
 
 
+#-------------------------------------------3.1 et 3.2
+
 
 labySimple = [[0, 0, 0, 0, 0, 0, 0],
-              [0, 2, 1, 1, 0, 1, 0],
-              [0, 0, 0, 1, 0, 1, 0],
-              [0, 0, 0, 1, 1, 3, 0],
+              [0, 1, 1, 1, 0, 3, 0],
+              [0, 1, 0, 1, 0, 1, 0],
+              [0, 2, 0, 1, 1, 1, 0],
               [0, 0, 0, 0, 0, 0, 0]]
+
+
+
+def afficherParcour(listCel, laby):  #cette fonction n'est pas demander dans le tp
+    for y in range(len(laby)):
+        print()
+        for x in range(len(laby[y])):
+            cel = (y, x)
+            if cel in listCel:
+                print("*", end=" ")
+            else:
+                print(laby[y][x], end=" ")
+    print()
+
 
 
 def exploreVoie(depart, labySimple):
@@ -104,11 +135,15 @@ def exploreVoie(depart, labySimple):
         for i in range(len(caseAcc)):
             if caseAcc[i] not in lstCellules:
                 case = caseAcc[i]
+        if case in lstCellules:
+            return []
         lstCellules += [(case)]
-        print(lstCellules)
+        afficherParcour(lstCellules, labySimple) #fait pas gaffe a ça c'est pas demander dans le tp c'est simplement pour afficher le chemin en cours c'était pour débuger
         if arrive == case:
             trouver = True
     return lstCellules
 
 
-print(exploreVoie(entree(labySimple), labySimple))
+
+
+print("Chemin pour sortir du labyrinthe : ", exploreVoie(entree(labySimple), labySimple))
