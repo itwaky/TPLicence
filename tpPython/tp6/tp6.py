a5c7db2a49d928994010f2e798c2e1f891906ab8
itwaky <eroscovic@gmail.com>
2022-10-25 11:42:37 +0200

    slt

--- tpPython/tp6/tp6.py
+++ tpPython/tp6/tp6.py
@@ -1,3 +1,7 @@
+import random
+
+
+
 #--------------------------------1
 
 
@@ -25,3 +29,84 @@ print(sommeUnSurNCarre(900))
 
 
 #--------------------------------3
+
+def listeAuHasard(n,valmax):
+    if n == 0:
+        return []
+    else:
+        return [random.randint(0,valmax)] + listeAuHasard(n-1,valmax)
+
+
+
+print(listeAuHasard(10,10))
+
+
+
+#---------------------------------------4
+
+
+lst = [0,1,2,3,4,5,6,7,8,9,10]
+
+def renverser(lst):
+    if lst == []:
+        return []
+    else:
+        return lst[-1:] + renverser(lst[:-1])
+
+
+print(renverser([0,1,2,3,4,5,6,7]))
+
+#----------------------------------5
+
+
+
+print(lst[:1])
+print("je m'apelle eros"[1:-1])
+print("je m'apelle eros"[:1])
+
+def chemin(lst, x):
+    if lst == []:
+        return []
+    elif x >= lst[0]:
+        return lst[:1] + chemin(lst[1:],x)
+    else :
+        return chemin(lst[1:],x)
+
+
+print(chemin(lst, 5))
+
+
+
+#---------------------------------------6
+
+def supprEspaceApost(txt):
+    if len(txt) == 0:
+        return ""
+    elif txt[0] == " " or txt[0] == "'":
+        return supprEspaceApost(txt[1:])
+    else:
+        return txt[:1] + supprEspaceApost(txt[1:])
+
+
+print(supprEspaceApost("je m'appelle eros"))
+
+
+
+#---------------------------------------7
+
+
+def estPalindrome(txt):
+    txt = supprEspaceApost(txt)
+    if len(txt) <= 1:
+        return True
+    elif txt[:1] == txt[-1:]:
+        return estPalindrome(txt[1:-1])
+    else:
+        return False
+
+
+print(estPalindrome("kay a k"))
+
+
+
+#--------------------------------------8
