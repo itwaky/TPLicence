c794cce25d0fba60fd23250ef8bfbe27ff2e16c4
itwaky <eroscovic@gmail.com>
2023-01-25 15:02:20 +0100

    morpion

--- tpPython/morpion.py
+++ tpPython/morpion.py
@@ -2,7 +2,7 @@ morpion = [[0,0,0],
            [0,0,0],
            [0,0,0]]
 
-#test
+gagnant = [0,1]
 
 def afficherMorpion(morpion):
     for ligne in range (len(morpion)):
@@ -17,14 +17,22 @@ def afficherMorpion(morpion):
     print(" ")
 
 def ajoutUtil(morpion, joueur ):
+    if joueur == 0:
+        print("Au tour des O...")
+    else:
+        print("Au tour des x...")
     y = int(input("Entrez votre choix en ordonné : "))
     x = int(input("Entrez votre choix en abscice : "))
-    if joueur == 0:
-        morpion[y][x] = 1
-    elif joueur == 1:
-        morpion[y][x] = 2
-    print(morpion)
-    return morpion
+    if morpion[y][x] == 1 or morpion[y][x] == 2:
+        input("la case est déjà occuper veillez appuyer sur entrer pour recomencer.")
+        ajoutUtil(morpion, joueur)
+    else:
+        if joueur == 0:
+            morpion[y][x] = 1
+        elif joueur == 1:
+            morpion[y][x] = 2
+        print(morpion)
+        return morpion
 
 
 victoire = False
@@ -38,9 +46,27 @@ while victoire == False:
     afficherMorpion(morpion)
     i += 1
     for y in range (len(morpion)):
-        if (morpion[y][0] == 1 and morpion[y][1] == 1 and morpion[y][2] == 1):
-            print("Bravo au joueur 0")
-            victoire = True
-        elif (morpion[0][y] == 1 and morpion[1][y] == 1 and morpion[2][y] == 1):
-            print("Bravo au joueur 0")
+        testLigne = morpion[y][0]
+        testColonne = morpion[0][y]
+        if testLigne != 0:
+            if (morpion[y][0] == testLigne and morpion[y][1] == testLigne and morpion[y][2] == testLigne):
+                if testLigne == 1:
+                    print("Bravo aux O")
+                else:
+                    print("Bravo aux x")
+                victoire = True
+        if testColonne != 0:
+            if (morpion[0][y] == testColonne and morpion[1][y] == testColonne and morpion[2][y] == testColonne):
+                if testColonne == 1:
+                    print("Bravo aux O")
+                else:
+                    print("Bravo aux x")
+                victoire = True
+    testDiagonale = morpion[1][1]
+    if testDiagonale != 0:
+        if (morpion[0][0] == testDiagonale and morpion[2][2] == testDiagonale or morpion[0][2] == testDiagonale and morpion[2][0] == testDiagonale):
+            if testDiagonale == 1:
+                print("Bravo aux O")
+            else:
+                print("Bravo aux x")
             victoire = True
