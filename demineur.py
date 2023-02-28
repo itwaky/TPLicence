8ec9aaa9c35b0b06b7c7ca9027cb83609772fb9d
itwaky <eroscovic@gmail.com>
2023-02-28 15:25:05 +0100

    slt

--- demineur.py
+++ demineur.py
@@ -21,18 +21,18 @@ def afficherMatrice (matrice):
 def generationMine (matrice):
     for x in range (len(matrice)):
         for y in range (len(matrice[x])):
-            if random.randint(1,20) == 1:
+            if random.randint(1,10) == 1:
                 matrice[x][y] = 9
     return matrice
 
 
 def calculAdjacent (matrice):
     for x in range(len(matrice)):
-        for y in range(len(matrice)):
+        for y in range(len(matrice[x])):
             if matrice[x][y] == 9:
-                for x2 in range (x-1, x+1):
-                    for y2 in range (y-1, y+1):
-                        if x2 >= 0 and y2 >= 0 and (x != x2 or y !=y2):
+                for x2 in range (x-1, x+2):
+                    for y2 in range (y-1, y+2):
+                        if ((x2 >= 0 and y2 >= 0) and (x != x2 or y !=y2) and (x2 < len(matrice) and y2 < len(matrice[x2])) ):
                             if matrice[x2][y2] != 9:
                                 matrice[x2][y2] = matrice[x2][y2] + 1
     return matrice
@@ -41,9 +41,11 @@ def calculAdjacent (matrice):
 def creationMatrice (tailleX, tailleY):
     matrice = []
     for x in range (tailleX):
-        matrice.append([0])
+        matrice.append([])
         for y in range (tailleY):
             matrice[x].append(0)
+    matrice = generationMine(matrice)
+    matrice = calculAdjacent(matrice)
     return matrice
 
 afficherMatrice(creationMatrice(10,10))
