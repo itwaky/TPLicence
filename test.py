65318266c1afba72359fdd0d1755f54cd4d4b7c5
itwaky <itwaky@pop-os.localdomain>
2023-05-16 13:35:17 +0200

    test

--- test.py
+++ test.py
@@ -2,19 +2,37 @@ from tkinter import *
 import subprocess
 import os
 
+courrant = os.getcwd()
+
+def clear_frame():
+   for widgets in fenetre.winfo_children():
+      widgets.destroy()
+
+def listButton(courrant):
+    ls = os.listdir(courrant)
+    for file in ls:
+        button = Button(fenetre, text=file, command= lambda texte=file: changerRepertoire(courrant, texte))
+        button.pack()
+
+def changerRepertoire(courrant, nom):
+    print(nom)
+    courrant = courrant + "/" + nom
+    print(courrant)
+    clear_frame()
+    listButton(courrant)
+
+
 
 
 fenetre = Tk()
+
+
+repertoire = Label(fenetre, text="Répertoire courant : "+courrant)
+
+repertoire.pack()
 #ls = subprocess.check_output(["ls"])
-ls = os.listdir()
-compteur = 1
-
-liste = Listbox(fenetre)
 
 
-for file in ls:
-    liste.insert(compteur, file)
-    compteur += 1
-liste.pack()
+listButton(courrant)
 
 fenetre.mainloop()
