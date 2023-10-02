b95217f56b6fc2edd72a759b5a81b0bbfe06f64e
itwaky <eroscovic@gmail.com>
2023-10-02 17:28:51 +0200

    algoProg2

---
+++ algoProg2/TD2.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+//----------------exercice5--------------------------
+
+
+
+
+
+int somme1(int n){
+    if (n == 0){
+        return 0;
+    }else{
+        return somme1(n-1) + n;
+    }
+}
+
+int sommeCarres(int n){
+    if (n == 0){
+        return 0;
+    }else{
+        return sommeCarres(n-1) + n * n;
+    }
+}
+
+
+int sommeImpaire(int n){
+    if (n == 0){
+        return 0;
+    }else if (n % 2 != 0){
+        return sommeImpaire(n-1) + n;
+    }else{
+        return sommeImpaire(n-1);
+    }
+}
+
+int sommePaire(int n){
+    if (n == 0){
+        return 0;
+    }else if (n % 2 == 0){
+        return sommePaire(n-1) + n;
+    }else{
+        return sommePaire(n-1);
+    }
+}
+
+float sommeDiv(int n){
+    if (n == 0){
+        return 0;
+    }else{
+        sommeDiv(n-1) + 1/n;
+    }
+}
+
+
+//-------------------exercice6----------------------
+
+
+int somme2recusif(int n){
+    if (n == 0){
+        return 0;
+    }else{
+        return somme2recusif(n-1) + somme1(n);
+    }
+}
+
+
+//---------------------exercice7---------------------
+
+// entre : entier n
+// sortie : void
+// Affiche un triangle isocèle comportant n ligne en entree 
+void dessineTriangle(int n){
+    for (int i = 0; i <= n; i++){
+        printf("\n");
+        for (int y = 0;y<= n; y++){
+            if (n - i <= y){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+    }
+    printf("\n");
+}
+
+int main(){
+    dessineTriangle(5);
+}
--- algoProg2/TP3.c
+++ algoProg2/TP3.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-
+// void toursHanoi(int n, char D, char A, char I) {
+//     if (n == 1)
+//       printf("Disque 1 de %c a %c \n" , D , A);
+//     else {
+//       // D à A
+//       toursHanoi(n - 1, D, I, A);
+//       printf("Disque %d de %c a %c \n", n , D ,A);
+//       //I à A
+//       toursHanoi(n - 1, I, A, D);
+//     }
+// }
 
 //-------------------------exercice1---------------------
 
@@ -52,6 +63,8 @@ int main(){
 //-------------------------exercice2---------------------------------
 
 
+/*
+
 int hasard(int min, int max){
     return (int) (min + ((float) rand() / RAND_MAX * (max - min + 1)));
 }
@@ -91,5 +104,61 @@ int main(){
         }
         joueur++;
     }
+    if (joueur % 2 != 0){
+        printf("Vous avez perdu.\n");
+    }else{
+        printf("Vous avez gagner.\n");
+    }
     
+}
+
+*/
+
+//------------------exercice3-------------------------
+
+/*
+
+int factoriel(int n){
+    if (n == 0 || n == 1){
+        return 1;
+    }else{
+        return factoriel(n-1) * n;
+    }
+}
+
+float kepler(int e){
+    if (e == 1 || e == 0){
+        return 2;
+    }else{
+        return kepler(e-1) + (float) 1/(factoriel(e));
+    }
+}
+
+int main(){
+    printf("%f\n", kepler(10));
+}
+*/
+
+
+
+//------------------------exercice4--------------------
+
+
+
+
+void hanoi(int n, int depart, int inter, int arrivee){
+    if (n == 1){
+        printf("Deplacement de la tour %d a la tour %d\n", depart, arrivee);
+    }else{
+        printf("Deplacement de la tour %d a la tour %d\n", inter, arrivee);
+        hanoi(n-1, depart, arrivee, inter);
+        printf("Deplacement de la tour %d a la tour %d\n", depart, inter);
+        hanoi(n-1,arrivee, inter, depart);
+    }
+}
+// trouver le millieu du problème 
+
+
+int main(){
+    hanoi(5, 1, 2, 3);
 }
