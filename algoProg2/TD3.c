a65073beea358c1031ba3762494199f139be5759
itwaky <eroscovic@gmail.com>
2023-10-16 17:20:58 +0200

    algoProg

--- algoProg2/TD3.c
+++ algoProg2/TD3.c
@@ -94,17 +94,173 @@ void permutationN(int tab[], int size, int n){
 
 //+32 
 
-void Maj(char str[], int size){
-    for(int i = 0;i>size;++i){
-        str[i] = str[i] - 32;
+void Maj(char str[]){
+    printf("Entrez une chaine : ");
+    scanf("%s", str);
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if(str[i] >= 'a' && str[i] <= 'i'){
+            str[i] = str[i - ('a' - 'A')];
+        }
+        i++;
+    }
+    
+}
+
+
+
+
+//----------------------------------exercice7-------------------------------
+
+
+int rechercheChar(const char str[], char character){
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == character){
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+    
+}
+
+int rechercheMot(const char str[], const char mot[]){
+    int i = 0;
+    while (mot[i] != '\0')
+    {
+        if (!rechercheChar(str, mot[i])){
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+    
+}
+
+
+void supprimeVoy(char str[]){
+    int p = 0;
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if(str[i] != 'a' || str[i] != 'e' || str[i] != 'i' || str[i] != 'o' || str[i] != 'u' || str[i] != 'y'){
+            str[p] == str[i];
+            p++;
+        }
+        i++;
+    }
+    str[p] = '\0';
+    
+}
+
+
+
+//------------------------exercice8--------------------------------
+
+
+
+void mirroir(int tab[],int size){
+    int temp = 0;
+    for(int i = 0; i< size / 2; i++){
+        temp = tab[i];
+        tab[i] = tab[size - i -1];
+        tab[size - i - 1] = temp;
+
     }
 }
 
-int main(){
-    char str[] = "bonjour";
-    Maj(str, 7);
-    for(int i = 0; i<7;++i){
-        printf("%c", str[i] - 32);
+
+// int main(){
+//     int tab[5] = {1,2,3,4,5};
+//     mirroir(tab, 5);
+//     for (int i = 0; i<5; ++i){
+//         printf("%d\t", tab[i]);
+//     }
+//     printf("\n");
+// }
+
+
+
+//------------------------------exercice9-----------------------------------
+
+
+int estPalindrome(char tab[]){
+    int size = 0;
+    int i = 0;
+    while (tab[size] != '\0')
+    {
+        size++;
     }
-    printf("\n");
+    
+    while (tab[i] != '\0')
+    {
+        if (tab[i] != tab[size - i - 1]){
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+    
 }
+
+
+// int main(){
+//     char str[] = "raar";
+//     printf("%d\n", estPalindrome(str));
+// }
+
+
+//--------------------------------exercice10------------------------------
+
+
+int estEgaux(int tab1[], int tab2[], int size){
+    for(int i = 0; i<size; ++i){
+        if (tab1[i] != tab2[i]){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+// int main(){
+//     int tab1[5] = {1,2,3,4,5};
+//     int tab2[5] = {1,2,3,5,5};
+//     printf("%d\n", estEgaux(tab1, tab2, 5));
+// }
+
+
+
+//--------------------------------exercice11-----------------------------
+
+
+int rechercheIndice(int tab[], int size, int elem){
+    for(int i = 0; i<size; ++i){
+        if (tab[i] == elem){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+//--------------------------------exercice12---------------------------
+
+
+int occurence(int tab[], int size, int elem){
+    int nb = 0;
+    for(int i = 0; i<size; ++i){
+        if(tab[i] == elem){
+            nb++;
+        }
+    }
+    return nb;
+}
+
+
+//-------------------------------exercice13-----------------------------
+
+
--- algoProg2/TP4.c
+++ algoProg2/TP4.c
@@ -3,9 +3,9 @@
 
 // ----------------------------1.a------------------------
 
-void afficher_tab(int tab[], int size){
+void afficher_tab(const int tab[], int size){
     for (int i = 0; i<size; ++i){
-        printf("| %d ", tab[i]);
+        printf("%d\t ", tab[i]);
     }
     printf("\n");
 }
@@ -33,5 +33,112 @@ void triBulles(int tab[], int size){
 //     triBulles(tab, 5);
 //     afficher_tab(tab, 5);
 //     return 0;
+// }
+
+
+
+// --------------------------2.a----------------------------
+
+
+// int maximum(int tab[], int size){
+//     int temp = tab[0];
+//     for(int i = 1; i<size ; ++i){
+//         if (temp<tab[i]){
+//             temp = tab[i];
+//         }
+//     }
+//     return temp;
+// }
+
+
+
+//---------------------------2.b--------------------------
+
+// int indiceMax(int tab[], int size){
+//     int temp = tab[0];
+//     int indiceMax = 0;
+//     for(int i = 1; i<size ; ++i){
+//         if (temp<tab[i]){
+//             temp = tab[i];
+//             indiceMax = i;
+//         }
+//     }
+//     return indiceMax;
+// 
+// }
+// 
+// 
+// int main(){
+//     int tab[5] = {2,8,2,3,4};
+//     int max = indiceMax(tab, 5);
+//     printf("%d\n", max);
+// }
+
+
+//------------------------------3-------------------------------
+
+
+//int rechercheWhile(int tab[],int size, int elem){
+//    int i = 0;
+//    while (i != size - 1)
+//    {
+//        if(tab[i] == elem){
+//            return i;
+//        }
+//        ++i;
+//    }
+//    
+//    return -1;
+//}
+
+//int recherche(int tab[],int size, int elem){
+//    for (int i = 0; i<size; ++i){
+//        if(tab[i] == elem){
+//            return i;
+//        }
+//    }
+//    return -1;
+//}
+// 
+// int main(){
+//     int tab[5] = {1,2,3,4,5};
+//     printf("%d\n", recherche(tab, 5, 7));
+// }
+
+
+
+//-------------------------------4-----------------------------------
+
+// remplace l'élément par 0 puis le déplace à la fin du tableau
+// paramètres : tableau flottant, taille du tableau de flottant -> entier, indice de l'élément a supprimer -> entier
+// sortie : void
+
+void supprimeTab(float tab[], int size, int elem){
+    for (int i = elem; i<size - 1; ++i){
+        tab[i] = tab[i+1];
+        if (i == size - 2){
+            tab[i+1] = 0;
+        }
+    }
+}
+
+
+int main(){
+    float tab[5] = {1,2,3,4,5};
+    supprimeTab(tab, 5, 3);
+}
+
+
+//---------------------------------5-------------------------------
+
+
+// void insertTab(float tab[],int pos, int elem){
+//     tab[pos] = elem;
+// }
+// 
+// 
+// int main(){
+//     float tab[5] = {1,2,3,4,5};
+//     insertTab(tab, 3, 10);
 // }
 
