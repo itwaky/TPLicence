9c2ce6a8e9398d36f5e1968ea75e8edbdf5c94a1
itwaky <eroscovic@gmail.com>
2023-10-24 09:53:36 +0200

    algoProg

--- algoProg2/TP5.c
+++ algoProg2/TP5.c
@@ -5,7 +5,7 @@
 
 //----------1
 
-void affiche(int matrice[3][3], int size){
+void affiche(int matrice[][3], int size){
     for(int i = 0; i<size; ++i){
         for(int j = 0; j<size; ++j){
             printf("%d ", matrice[i][j]);
@@ -16,15 +16,15 @@ void affiche(int matrice[3][3], int size){
 }
 
 
-int main(){
-    int matrice[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    affiche(matrice, 3);
-}
+// int main(){
+//     int matrice[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+//     affiche(matrice, 3);
+// }
 
 //-------2
 
 
-int nombreDeZeros(int matrice[3][3], int size){
+int nombreDeZeros(int matrice[][3], int size){
     int total = 0;
     for(int i = 0; i<size; ++i){
         for(int j = 0; j<size; ++j){
@@ -39,7 +39,7 @@ int nombreDeZeros(int matrice[3][3], int size){
 
 //-----3
 
-int estDiagonale(int matrice[3][3], int size){
+int estDiagonale(int matrice[][3], int size){
     int diag = matrice[0][0];
     for(int i = 0; i<size; ++i){
         for(int j = 0; j<size; ++j){
@@ -54,3 +54,53 @@ int estDiagonale(int matrice[3][3], int size){
 
 //------4
 
+
+int estSymetrique(int matrice[][3], int size){
+    for(int i = 0; i<size; ++i){
+        for(int j = 0; j<size; ++j){
+            if(matrice[i][j] != matrice[j][i]){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+
+// int main(){
+//     int matrice[3][3] = {{1,4,7},
+//                          {4,5,8},
+//                          {7,8,9}};
+//     affiche(matrice, 3);
+//     printf("La matrice est elle symetrique : %d \n", estSymetrique(matrice, 3));
+// }
+
+
+//--------5
+
+void transpose(int matrice[][3], int size){
+    int temp = 0;
+    for(int i = 0; i<size; ++i){
+        for(int j = 0; j<size; ++j){
+            // la deuxième partie de la matrice se recopie une deuxième fois a coriger
+            temp = matrice[i][j];
+            matrice[i][j] = matrice[j][i];
+            matrice[j][i] = temp;
+            printf("%d ",matrice[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+int main(){
+    int matrice[3][3] = {{1,4,8},
+                         {5,5,8},
+                         {7,8,9}};
+    affiche(matrice, 3);
+    
+    transpose(matrice, 3);
+
+    affiche(matrice, 3);
+}
