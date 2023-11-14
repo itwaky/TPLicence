381869f38c2a2ba032c04a0696b69cb268f54cf9
itwaky <eroscovic@gmail.com>
2023-11-14 16:15:30 +0100

    algoProg

--- algoProg2/entrainementCC2.c
+++ algoProg2/entrainementCC2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include <string.h>
+
 
 //------------------------CC2--2022------------------------------
 
@@ -54,6 +56,93 @@ int noteAdn(char ADN[]){
     return note;
 }
 
+
+
+//------------exercice2
+
+//----2.1
+
+
+typedef struct
+{
+    int jour;
+    int mois;
+    int annee;
+}Date;
+
+typedef struct
+{
+    int numLocation;
+    Date dateDebut;
+    Date dateFin;
+}Contrat;
+
+typedef struct
+{
+    char modele[20];
+    int nImma;
+    int kilometrage;
+    int dispo;
+    Contrat contratLocation;
+}Voiture;
+
+
+Voiture initVoiture(char modele[], int imma, int kilometrage){
+    Voiture v;
+    strcpy(v.modele, modele);
+    v.nImma = imma;
+    v.kilometrage = kilometrage;
+    v.dispo = 1;
+    Date debut = {0,0,0};
+    Date fin = {0,0,0};
+    Contrat c = {0, debut, fin};
+    v.contratLocation = c;
+    return v;
+}
+
+void afficheVoiture(Voiture v){
+    int i = 0;
+    printf("Modele : ");
+    while(v.modele[i] != '\0'){
+        printf("%c", v.modele[i]);
+        i++;
+    }
+    printf(" numéros immatriculation : %d, kilometrage : %d, dispo : %d\n", v.nImma, v.kilometrage, v.dispo);
+}
+
+void afficheVoitureContrat(Voiture v){
+    afficheVoiture(v);
+    printf("Contrat : ");
+    Contrat c = v.contratLocation;
+    Date dateDebut = c.dateDebut;
+    Date dateFin = c.dateFin;
+    printf("nContrat : %d, date début : %d/%d/%d, date fin : %d/%d/%d \n\n", c.numLocation, dateDebut.jour, dateDebut.mois, dateDebut.annee, dateFin.jour, dateFin.mois, dateFin.annee);
+
+}
+
+
+void dispo(const Voiture tab[], int size){
+    printf("Modèles disponibles : \n");
+    for(int i = 0; i<size; i++){
+        if(tab[i].dispo){
+            afficheVoiture(tab[i]);
+        }
+    }
+
+}
+
+
+
+Voiture nouveau_contrat(Voiture v, Date dateDebut, Date dateFin){
+    static int id = 1;
+    v.dispo = 0;
+    Contrat c = {id, dateDebut, dateFin};
+    v.contratLocation = c;
+    id++;
+    return v;
+}
+
+
 int main(){
     // 1.1
     // char adnVrai[] = "AACTTGGCATGA";
@@ -70,7 +159,36 @@ int main(){
     // char adn[] = "AATGC";
     // printf("Note : %d\n", noteAdn(adn));
 
+    // 2.2
+    char modele[] = "toyota";
+    int nImma = 123456789;
+    int kilometrage = 100000;
+    Voiture v = initVoiture(modele, nImma, kilometrage);
+    // afficheVoiture(v);
     
+
+    char modele2[] = "audi";
+    int nImma2 = 987654321;
+    int kilometrage2 = 9999;
+    Voiture v2 = initVoiture(modele2, nImma2, kilometrage2);
+    // Voiture tab[] = {v, v2};
+    // dispo(tab, 2);
+
+    afficheVoitureContrat(v);
+
+    Date dateDebut = {12,01,2019};
+    Date dateFin = {20,01,2019};
+    v = nouveau_contrat(v, dateDebut, dateFin);
+
+    afficheVoitureContrat(v);
+
+    Date dateDebut2 = {21,02, 2019};
+    Date dateFin2 = {28, 02, 2019};
+    v = nouveau_contrat(v, dateDebut2, dateFin2);
+
+    afficheVoitureContrat(v);
+
+
 }
 
 
