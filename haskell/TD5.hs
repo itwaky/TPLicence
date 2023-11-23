0b4e79463cb2920ecf187208525186158d7b97a8
itwaky <eroscovic@gmail.com>
2023-11-23 09:29:43 +0100

    haskell

--- haskell/TD5.hs
+++ haskell/TD5.hs
@@ -8,8 +8,18 @@ nth l1 n = case l1 of {
     t:r ->  if n == 0
                 then t
             else
-                nth l1 (n-1)
-    [] -> []
+                nth r (n-1);
+    [] -> 0
+}
+
+-- 2
+
+sans_doublon l = case l of{
+    [] -> [];
+    t:r ->  if t == head r
+                then sans_doublon r
+            else
+                sans_doublon r ++ [r]
 }
 
 
