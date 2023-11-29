c7d232eb56eb2fec2af6febb1ac17eb71459876f
itwaky <eroscovic@gmail.com>
2023-11-30 00:28:55 +0100

    algoProg2

--- algoProg2/TP8.c
+++ algoProg2/TP8.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 
 //---1
@@ -27,7 +28,7 @@
 //-------------Mise en oeuvre-----------
 
 
-//1
+//------------------------1
 
 char** allouer_sapin (int h, int l){
     char** hauteur = (char**) malloc(sizeof(char*)*h);
@@ -40,16 +41,16 @@ char** allouer_sapin (int h, int l){
 }
 
 
-//2
-void liberer_sapin(char **m,int l){
-    for(int i = 0; i<l; i++){
+//-------------------------2
+void liberer_sapin(char** m,int h){
+    for(int i = 0; i<h; i++){
         free(*(m + i));
     }
     free(m);
 }
 
 
-//3
+//--------------------------3
 
 
 void initialiser_sapin(char** sapin, int h, int l, int hb){
@@ -75,6 +76,9 @@ void initialiser_sapin(char** sapin, int h, int l, int hb){
     }
 }
 
+
+//---------------------4
+
 void afficher_sapin(char** sapin, int h, int l){
     for(int i = 0; i<h; i++){
         for(int j = 0; j<l; j++){
@@ -84,18 +88,86 @@ void afficher_sapin(char** sapin, int h, int l){
     }
 }
 
+//---------------5
+
+
+//4
+
+
+
+//----------------------Partie2------------------------
+
+
+//------------------1
+
 typedef struct{
     int x;
     int y;
-}Position;
+}Position_t;
 
-int positionValide(char** sapin, Position p){
+
+//-----------------------2
+
+int positionValide(char** sapin, Position_t p){
     if (sapin[p.x][p.y] == '*'){
         return 1;
     }
     return 0;
 }
 
+
+
+//------------------------3
+
+int aleatoire(int min, int max){
+    static int rand_init = 0;
+    if(!rand_init){
+        srand(time(NULL));
+        rand_init = 1;
+    }
+    return rand()%(max-min) + min;
+}
+
+
+void mystere(Position_t* deco, int nb_boules, char** sapin, int h_branches, int largeur){
+    int ligne = 0;
+    int colonne = 0;
+    for(int i = 0; i<nb_boules;i++){
+        do{
+            ligne = aleatoire(0, h_branches);
+            colonne = aleatoire(0, largeur);
+            deco[i].x = ligne;
+            deco[i].y = colonne;
+        }while(!positionValide(sapin, deco[i]));
+    }
+    return;
+}
+
+// la fonction mystere retourne un tableau de position_t contenant les position valide pour les décorations
+
+void afficheTab(Position_t* deco, int size){
+    printf("indice boules : ");
+    for(int i = 0; i<size; i++){
+        printf("(%d,%d) ", deco[i].x, deco[i].y);
+    }
+    printf("\n");
+    return;
+}
+
+
+//--------------------------------------------------------4
+
+
+void decorer_sapin(char** sapin, Position_t* deco, int sizeDeco){
+    for (int i = 0; i<sizeDeco; i++){
+        sapin[deco[i].x][deco[i].y] = 'o';
+    }
+    return;
+}
+
+
+
+//-------------------------------5
 
 
 int main(){
@@ -106,14 +178,27 @@ int main(){
     int l = hb * 2;
     char** sapin = allouer_sapin(h,l);
     initialiser_sapin(sapin, h,l,hb);
-    afficher_sapin(sapin, h, l);
+    //afficher_sapin(sapin, h, l);
+
+
+    int qteBoules = 0;
+    printf("Entrez la quantité de boules : ");
+    scanf("%d", &qteBoules);
+
+
+    Position_t* deco = (Position_t*) malloc(sizeof(Position_t)*qteBoules);
+    mystere(deco, qteBoules, sapin, hb, l);
+    afficheTab(deco, qteBoules);
+
+
+
+    decorer_sapin(sapin, deco, qteBoules);
+    afficher_sapin(sapin,h,l);
+
+    free(deco);
+    liberer_sapin(sapin, h);
 }
 
-
-//---------------5
-
-
-//4
 
 
 
